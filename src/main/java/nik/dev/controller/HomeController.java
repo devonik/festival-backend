99fd2e2a6dfe7aae36e23430fdb6204bfa77f1bb
dragoono <niklas.grieger1@gmail.com>
2018-02-23 14:29:02 +0100

    first commit

---
+++ src/main/java/nik/dev/controller/HomeController.java
@@ -0,0 +1,13 @@
+package nik.dev.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String home() {
+		return "festivalticker, BACKEND!!!";
+	}
+}
---
+++ src/main/java/nik/dev/model/Festival.java
@@ -0,0 +1,5 @@
+package nik.dev.model;
+
+public class Festival {
+
+}
