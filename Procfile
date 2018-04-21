3ac9f09d117212cce1645e518f5f459832b094ad
Niklas Grieger <niklas.grieger1@gmail.com>
2018-04-21 14:06:49 +0200

    timezone

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java -Dserver.port=$PORT $JAVA_OPTS -jar target/*.jar
+web: java -Duser.timezone=CEST -Dserver.port=$PORT $JAVA_OPTS -jar target/*.jar
--- src/main/java/nik/dev/App.java
+++ src/main/java/nik/dev/App.java
@@ -1,5 +1,9 @@
 package nik.dev;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +12,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @SpringBootApplication
 public class App 
 {
+	@PostConstruct
+	  void started() {
+	    TimeZone.setDefault(TimeZone.getTimeZone("CEST"));
+	}
     public static void main( String[] args )
     {
         SpringApplication.run(App.class, args);
