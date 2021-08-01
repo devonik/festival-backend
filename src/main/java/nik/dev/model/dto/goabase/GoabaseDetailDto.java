cc27cef2a3d99aad677599557ff85d31e6518b9d
Niklas Grieger <niklas.grieger1@gmail.com>
2021-08-01 23:57:43 +0200

    add goabase detail infos

---
+++ src/main/java/nik/dev/model/dto/goabase/GoabaseDetailDto.java
@@ -0,0 +1,15 @@
+package nik.dev.model.dto.goabase;
+
+import java.util.ArrayList;
+
+public class GoabaseDetailDto {
+    private GoabasePartyDetailDto party;
+
+    public GoabasePartyDetailDto getParty() {
+        return party;
+    }
+
+    public void setParty(GoabasePartyDetailDto party) {
+        this.party = party;
+    }
+}
--- src/main/java/nik/dev/service/GoabaseService.java
+++ src/main/java/nik/dev/service/GoabaseService.java
@@ -2,10 +2,15 @@ package nik.dev.service;
 
 import nik.dev.model.Festival;
 import nik.dev.model.FestivalDetail;
+import nik.dev.model.FestivalTicketPhase;
+import nik.dev.model.dto.goabase.GoabaseDetailDto;
 import nik.dev.model.dto.goabase.GoabaseListDto;
+import nik.dev.model.dto.goabase.GoabasePartyDetailDto;
 import nik.dev.model.dto.goabase.GoabasePartyDto;
 import nik.dev.repository.IFestivalDetailRepository;
 import nik.dev.repository.IFestivalRepository;
+import nik.dev.repository.IFestivalTicketPhaseRepository;
+import org.apache.commons.text.StringEscapeUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.stereotype.Service;
@@ -33,101 +38,123 @@ public class GoabaseService {
     @Autowired
     IFestivalDetailRepository festivalDetailRepository;
 
+    @Autowired
+    IFestivalTicketPhaseRepository festivalTicketPhaseRepository;
+
     @PersistenceContext
     private EntityManager entityManager;
 
-    public void checkGoabaseParties(){
+    public void checkGoabaseParties() {
         ArrayList<GoabasePartyDto> goabaseParties = this.fetchGoabaseParties("DE", "festival", "festival");
 
-        for(GoabasePartyDto goabaseParty : goabaseParties){
-            this.insertOrUpdateParty(goabaseParty);
+        for (GoabasePartyDto goabaseParty : goabaseParties) {
+            this.insertOrUpdateParty(this.fetchGoabaseParty(goabaseParty.getId()));
         }
     }
 
-    private void insertOrUpdateParty(GoabasePartyDto goabaseParty){
-            //Thumbnail can not be null yet so ignore the entry
-            if(goabaseParty.getUrlImageFull() == null) return;
+    private void insertOrUpdateParty(GoabaseDetailDto goabaseDetail) {
+        GoabasePartyDetailDto goabasePartyDetail = goabaseDetail.getParty();
+        //Thumbnail can not be null yet so ignore the entry
+        if (goabasePartyDetail.getUrlImageFull() == null) return;
 
-            Optional<Festival> festival = festivalRepository.findByGoabaseId(goabaseParty.getId());
+        Optional<Festival> festival = festivalRepository.findByGoabaseId(goabasePartyDetail.getId());
 
-            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssX");
-            try {
-                Date startDate = formatter.parse(goabaseParty.getDateStart());
-                Date endDate = formatter.parse(goabaseParty.getDateEnd());
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssX");
+        try {
+            Date startDate = formatter.parse(goabasePartyDetail.getDateStart());
+            Date endDate = formatter.parse(goabasePartyDetail.getDateEnd());
 
-                if (festival.isPresent()) {
-                    if (goabaseParty.getNameStatus().equals("Cancelled")) {
-                        festivalRepository.delete(festival.get());
+            if (festival.isPresent()) {
+                if (goabasePartyDetail.getNameStatus().equals("Cancelled")) {
+                    festivalRepository.delete(festival.get());
+                } else {
+                    //Festival is present - need to update
+                    FestivalDetail festivalDetail = null;
+                    Optional<FestivalDetail> existingFestivalDetail = festivalDetailRepository.findByFestivalId(festival.get().getFestival_id());
+                    if (existingFestivalDetail.isPresent()) {
+                        festivalDetail = existingFestivalDetail.get();
+                        if (festival.get().getFestival_detail_id() == null)
+                            festival.get().setFestival_detail_id(existingFestivalDetail.get().getFestival_detail_id());
                     } else {
-                        //Festival is present - need to update
-                        FestivalDetail festivalDetail = null;
-                        Optional<FestivalDetail> existingFestivalDetail = festivalDetailRepository.findByFestivalId(festival.get().getFestival_id());
-                        if(existingFestivalDetail.isPresent()){
-                            festivalDetail = existingFestivalDetail.get();
-                            if(festival.get().getFestival_detail_id() == null ) festival.get().setFestival_detail_id(existingFestivalDetail.get().getFestival_detail_id());
-                        }else{
-                            festivalDetail = new FestivalDetail();
-                        }
-                        festival.get().setGoabaseId(goabaseParty.getId());
-                        festival.get().setName(goabaseParty.getNameParty());
-                        festival.get().setDatum_start(startDate);
-                        festival.get().setDatum_end(endDate);
-                        festival.get().setThumbnail_image_url(goabaseParty.getUrlImageFull());
-                        Festival updatedFestival = festivalRepository.save(festival.get());
+                        festivalDetail = new FestivalDetail();
+                    }
+                    festival.get().setGoabaseId(goabasePartyDetail.getId());
+                    festival.get().setName(goabasePartyDetail.getNameParty());
+                    festival.get().setDatum_start(startDate);
+                    festival.get().setDatum_end(endDate);
+                    festival.get().setThumbnail_image_url(goabasePartyDetail.getUrlImageFull());
+                    Festival updatedFestival = festivalRepository.save(festival.get());
 
-                        String homepageUrl = null;
-                        if(goabaseParty.getUrlOrganizer() != null) homepageUrl = "<a href='"+goabaseParty.getUrlOrganizer()+"'>Homepage</a>";
-                        if(goabaseParty.getUrlOrganizer() == null && goabaseParty.getUrlPartyHtml() != null) homepageUrl = "<a href='"+goabaseParty.getUrlPartyHtml()+"'>Homepage</a>";
-
-                        festivalDetail.setFestivalId(updatedFestival.getFestival_id());
-                        festivalDetail.setGeoLatitude(Double.parseDouble(goabaseParty.getGeoLat()));
-                        festivalDetail.setGeoLongitude(Double.parseDouble(goabaseParty.getGeoLon()));
-                        festivalDetail.setHomepage_url(homepageUrl);
-
-                        festivalDetailRepository.save(festivalDetail);
-
+                    festivalDetail.setFestivalId(updatedFestival.getFestival_id());
+                    festivalDetail.setGeoLatitude(Double.parseDouble(goabasePartyDetail.getGeoLat()));
+                    festivalDetail.setGeoLongitude(Double.parseDouble(goabasePartyDetail.getGeoLon()));
+                    try {
+                        String description = StringEscapeUtils.escapeJava(
+                                goabasePartyDetail.getTextMore() +
+                                        "\n\nWird organisiert von " +
+                                        goabasePartyDetail.getNameOrganizer() +
+                                        "\n" +
+                                        goabasePartyDetail.getTextOrganizer());
+                        festivalDetail.setDescription(description);
+                    } catch (Exception ex) {
+                        System.out.println("ERROR - Could not extract goabase description: " + ex.getMessage());
                     }
 
-                } else if (!goabaseParty.getNameStatus().equals("Cancelled")) {
-                    //Festival is new - need to insert
-                    Festival newFestival = new Festival();
-                    newFestival.setGoabaseId(goabaseParty.getId());
-                    newFestival.setName(goabaseParty.getNameParty());
-                    newFestival.setDatum_start(startDate);
-                    newFestival.setDatum_end(endDate);
-                    newFestival.setThumbnail_image_url(goabaseParty.getUrlImageFull());
-                    Festival createdFestival = festivalRepository.save(newFestival);
+                    festivalDetail.setHomepage_url("<a href='" + goabasePartyDetail.getUrlParty() + "'>Homepage</a>");
 
-                    FestivalDetail newFestivalDetail = new FestivalDetail();
-                    String homepageUrl = null;
-                    if(goabaseParty.getUrlOrganizer() != null) homepageUrl = goabaseParty.getUrlOrganizer();
-                    if(goabaseParty.getUrlOrganizer() == null && goabaseParty.getUrlPartyHtml() != null) homepageUrl = goabaseParty.getUrlPartyHtml();
+                    festivalDetailRepository.save(festivalDetail);
 
-                    newFestivalDetail.setFestivalId(createdFestival.getFestival_id());
-                    newFestivalDetail.setGeoLatitude(Double.parseDouble(goabaseParty.getGeoLat()));
-                    newFestivalDetail.setGeoLongitude(Double.parseDouble(goabaseParty.getGeoLon()));
-                    newFestivalDetail.setHomepage_url(homepageUrl);
-
-                    FestivalDetail createdFestivalDetail = festivalDetailRepository.save(newFestivalDetail);
-
-                    if(createdFestival.getFestival_detail_id() == null){
-
-                        createdFestival.setFestival_detail_id(createdFestivalDetail.getFestival_detail_id());
-                        festivalRepository.save(createdFestival);
-                    }
                 }
-            } catch (Exception ex) {
-                System.out.println("ERROR - could not parse start & end date from goabase api. error:" + ex);
+
+            } else if (!goabasePartyDetail.getNameStatus().equals("Cancelled")) {
+                //Festival is new - need to insert
+                Festival newFestival = new Festival();
+                newFestival.setGoabaseId(goabasePartyDetail.getId());
+                newFestival.setName(goabasePartyDetail.getNameParty());
+                newFestival.setDatum_start(startDate);
+                newFestival.setDatum_end(endDate);
+                newFestival.setThumbnail_image_url(goabasePartyDetail.getUrlImageFull());
+                Festival createdFestival = festivalRepository.save(newFestival);
+
+                FestivalDetail newFestivalDetail = new FestivalDetail();
+
+                newFestivalDetail.setFestivalId(createdFestival.getFestival_id());
+                newFestivalDetail.setGeoLatitude(Double.parseDouble(goabasePartyDetail.getGeoLat()));
+                newFestivalDetail.setGeoLongitude(Double.parseDouble(goabasePartyDetail.getGeoLon()));
+
+                try {
+                    String description = StringEscapeUtils.escapeJava(
+                            goabasePartyDetail.getTextMore() +
+                            "\n\nWird organisiert von " +
+                            goabasePartyDetail.getNameOrganizer() +
+                            "\n" +
+                            goabasePartyDetail.getTextOrganizer());
+                    newFestivalDetail.setDescription(description);
+                } catch (Exception ex) {
+                    System.out.println("ERROR - Could not extract goabase description: " + ex.getMessage());
+                }
+                newFestivalDetail.setHomepage_url("<a href='" + goabasePartyDetail.getUrlParty() + "'>Homepage</a>");
+
+                FestivalDetail createdFestivalDetail = festivalDetailRepository.save(newFestivalDetail);
+
+                if (createdFestival.getFestival_detail_id() == null) {
+
+                    createdFestival.setFestival_detail_id(createdFestivalDetail.getFestival_detail_id());
+                    festivalRepository.save(createdFestival);
+                }
             }
+        } catch (Exception ex) {
+            System.out.println("ERROR - could not parse data from goabase. error:" + ex);
         }
+    }
 
 
     private ArrayList<GoabasePartyDto> fetchGoabaseParties(String country, String eventtype, String search) {
-        if(country == null) country = "DE";
-        if(eventtype == null) eventtype = "festival";
-        if(search == null) search = "festival";
+        if (country == null) country = "DE";
+        if (eventtype == null) eventtype = "festival";
+        if (search == null) search = "festival";
 
-        final String getGoabaseFestivalsURL = "https://goabase.net/api/party/json/?country="+country+"&eventtype="+eventtype+"&search=" + search;
+        final String getGoabaseFestivalsURL = "https://goabase.net/api/party/json/?country=" + country + "&eventtype=" + eventtype + "&search=" + search;
 
         RestTemplate restTemplate = new RestTemplate();
 
@@ -138,9 +165,24 @@ public class GoabaseService {
 
         GoabaseListDto goabaseList = restTemplate.getForObject(getGoabaseFestivalsURL, GoabaseListDto.class);
 
-        System.out.println(goabaseList.getPartylist().size());
-
         assert goabaseList != null;
         return goabaseList.getPartylist();
+    }
+
+    private GoabaseDetailDto fetchGoabaseParty(Long id) {
+        final String url = "https://goabase.net/api/party/json/" + id;
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        ArrayList<ClientHttpRequestInterceptor> interceptors = new ArrayList<>();
+
+        interceptors.add(new HeaderRequestInterceptor("Content-Type", "application/json"));
+        restTemplate.setInterceptors(interceptors);
+
+        GoabaseDetailDto goabaseDetail = restTemplate.getForObject(url, GoabaseDetailDto.class);
+
+        System.out.println("goabaseDetail: " + goabaseDetail.toString());
+
+        return goabaseDetail;
     }
 }
